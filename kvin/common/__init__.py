36b53e16071efa6f9464bd6d72c4670c2637c828
msnr-a <101387735+msnr-a@users.noreply.github.com>
2022-05-06 15:02:28 +0900

    Update __init__.py

--- kvin/common/__init__.py
+++ kvin/common/__init__.py
@@ -7,3 +7,9 @@ def wraps(wrapped, assigned = WRAPPER_ASSIGNMENTS + ("__args__",), updated = WRA
   
 from kvin.common import __define__
 def define(name, default=None, readonly=False, validation=None, on_value_changed=None): return __define__.define(name, default, readonly, validation, on_value_changed)
+
+from kvin.common import __validator__
+def validate(Type: type, arg: str, **kwargs): return __validator__.validate(Type, arg, **kwargs)
+
+from kvin.common import __base__
+def base(*args): return __base__.base(*args)
