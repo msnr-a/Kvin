a9bc267fd47af57549be1947519785816ef4002b
msnr-a <101387735+msnr-a@users.noreply.github.com>
2022-05-06 14:30:28 +0900

    Create __init__.py

---
+++ kvin/common/__init__.py
@@ -0,0 +1,9 @@
+from functools import partial, update_wrapper, WRAPPER_ASSIGNMENTS, WRAPPER_UPDATES
+
+def wraps(wrapped, assigned = WRAPPER_ASSIGNMENTS + ("__args__",), updated = WRAPPER_UPDATES):
+    if not hasattr(wrapped, "__args__"):
+        setattr(wrapped, "__args__", wrapped.__code__.co_varnames[:wrapped.__code__.co_argcount])
+    return partial(update_wrapper, wrapped=wrapped, assigned=assigned, updated=updated)
+  
+from common import define
+def define(name, default=None, readonly=False, validation=None, on_value_changed=None): return define.define(name, default, readonly, validation, on_value_changed)
